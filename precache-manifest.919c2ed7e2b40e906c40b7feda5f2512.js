bde5d3b02750f49648b39718df5380c58eb656d6
sidharth3000 <2019522342iiitvadodara.ac.in>
2020-05-13 01:11:33 +0530

    Updates

---
+++ precache-manifest.919c2ed7e2b40e906c40b7feda5f2512.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2f8883a8b9c95b5ff7c314c6cfa51c",
+    "url": "/ezewev/index.html"
+  },
+  {
+    "revision": "9db73e46b50f43b9bb58",
+    "url": "/ezewev/static/css/main.2982e95d.chunk.css"
+  },
+  {
+    "revision": "f3c5d3bbc27b01be33c6",
+    "url": "/ezewev/static/js/2.a59de1d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ezewev/static/js/2.a59de1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db73e46b50f43b9bb58",
+    "url": "/ezewev/static/js/main.ac2eab51.chunk.js"
+  },
+  {
+    "revision": "6b391dd8112509e1fb66",
+    "url": "/ezewev/static/js/runtime-main.0868a732.js"
+  },
+  {
+    "revision": "83b61d3f45b60be43f3d2a536d95fc8e",
+    "url": "/ezewev/static/media/Custom.83b61d3f.png"
+  },
+  {
+    "revision": "15c38f512e23447c74aa27401e13a3d9",
+    "url": "/ezewev/static/media/Des.15c38f51.png"
+  },
+  {
+    "revision": "297bbc78b80f798b63bd3d602946162e",
+    "url": "/ezewev/static/media/Envato.297bbc78.png"
+  },
+  {
+    "revision": "6e7e5bf03d4b4ee3ad913c700e512721",
+    "url": "/ezewev/static/media/Gear.6e7e5bf0.png"
+  },
+  {
+    "revision": "25195f5faeb5c6c0255f6cd3f91c290a",
+    "url": "/ezewev/static/media/Girl.25195f5f.png"
+  },
+  {
+    "revision": "907f0c331b116d6abf846c57db2fa54e",
+    "url": "/ezewev/static/media/Guys.907f0c33.png"
+  },
+  {
+    "revision": "7ff31d5531082cbe2b4b136fa64f9d00",
+    "url": "/ezewev/static/media/Logo.7ff31d55.png"
+  },
+  {
+    "revision": "0acef94ee8e530387ea124736dcffa88",
+    "url": "/ezewev/static/media/LogoDark.0acef94e.png"
+  },
+  {
+    "revision": "90802a8124546e7b74734d50e9c7d4a2",
+    "url": "/ezewev/static/media/Man.90802a81.png"
+  },
+  {
+    "revision": "cf0d04180de1b17f2392a0eea00a1526",
+    "url": "/ezewev/static/media/Ok.cf0d0418.png"
+  },
+  {
+    "revision": "d1c40b81ad698b03e8072ca221454a4a",
+    "url": "/ezewev/static/media/Shield.d1c40b81.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ezewev/static/media/logo.5d5d9eef.svg"
+  }
+]);
